3c0b2cace203f64b68da729d92087b0ea2b2350c
Jean Prado <jeanfprado@gmail.com>
2017-12-21 11:05:29 -0200

    Finalização

--- edit.blade.php
+++ edit.blade.php
@@ -1,20 +1,16 @@
 <div class="modal-header">
   <button type="button" class="close" data-dismiss="modal">&times;</button>
-  <h4 id='client_edit_title' class="modal-title">Editando Registro</h4>
+  <h4 id='user_edit_title' class="modal-title">Editando Registro</h4>
 </div>
 <div class="modal-body">
 
-  {!! Form::model($mclient, ['route'=>['admin.modules.clients.update', $mclient->id],'files' => true])  !!}
+  {!! Form::model($users, ['route'=>['users.update', $users->id],'files' => true])  !!}
 
   <div class="form-group" >
-    {!! Form::label('Type', 'Tipo:') !!}
-    {!! Form::select('type', ['Imagem', 'Arquivos'],null, ['class'=>'form-control', 'disabled']) !!}
+    {!! Form::label('Name', 'Name:') !!}
+    {!! Form::text('name', null, ['class'=>'form-control']) !!}
   </div>
 
-  @include('admin.modules.clients._form')
-  <div class="form-group" >
-    {!! Form::hidden('client_id',null) !!}
-  </div>
   <div class="form-group" >
 
     {!! Form::submit('Atualizar', ['class'=>'btn btn-primary']) !!}
